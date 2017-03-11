2d785cd0dcefbecfef49a730172f169b257b878d
eriol4ik <eriol4ik@gmail.com>
2017-03-11 18:13:58 +0200

    Logic changes

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -1,15 +1,23 @@
 package web.controller;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import web.entity.TicketForm;
 
 @Controller
+@Repository
 @RequestMapping(value = "/")
 @SessionAttributes(types = TicketForm.class)
 public class MainController {
+    @Autowired
+    SessionFactory sessionFactory;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String start(TicketForm ticketForm) {
@@ -19,6 +27,9 @@ public class MainController {
 
     @RequestMapping(value = "/index", method = RequestMethod.POST)
     public String index(TicketForm ticketForm) {
+        if (ticketForm.getTicketNumber() > 10 || ticketForm.getTicketNumber() < 1) {
+            return "error";
+        }
         return "name_surname";
     }
 
@@ -28,14 +39,15 @@ public class MainController {
         return "credit";
     }
 
+    @Transactional
     @RequestMapping(value = "/credit", method = RequestMethod.POST)
     public String credit(TicketForm ticketForm) {
+        try {
+            System.out.println(sessionFactory.getCurrentSession().save(ticketForm));
+        } catch (HibernateException he) {
+            return "error";
+        }
         return "complete";
     }
 
-    @RequestMapping(value = "/complete", method = RequestMethod.GET)
-    public String complete(TicketForm ticketForm) {
-
-        return "complete";
-    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: eriol4ik
+  Date: 11.03.2017
+  Time: 17:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+Вы не оформили заказ :(
+</body>
+</html>
