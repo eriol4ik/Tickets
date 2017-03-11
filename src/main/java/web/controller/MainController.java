a9f3e6044783059f8f9d9639ee81ec188540bf84
eriol4ik <eriol4ik@gmail.com>
2017-03-11 17:05:16 +0200

    Controller + Form

---
+++ src/main/java/web/controller/MainController.java
@@ -0,0 +1,7 @@
+package web.controller;
+
+/**
+ * Created by eriol4ik on 11.03.2017.
+ */
+public class MainController {
+}
---
+++ src/main/java/web/form/UserForm.java
@@ -0,0 +1,7 @@
+package web.form;
+
+/**
+ * Created by eriol4ik on 11.03.2017.
+ */
+public class UserForm {
+}
